5644adb89259539eb1f3879fc04f16ebcda2053d
Ali Abu Liel <>
2025-09-29 14:27:23 +0300

    delte files

--- tests/src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""TTM-VitalDB: Foundation Model for Biosignals."""
-
-__version__ = "0.1.0"
-
-from . import data, eval, models, utils
-
-__all__ = ["data", "eval", "models", "utils", "__version__"]
